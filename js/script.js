73cff6c120f63690b043304c4793b475a145d65b
Glitch (obtainable-deluxe-firewall) <none>
2022-02-24 01:57:44 +0000

    📰🥉 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 // Complete Day 1 goals here
-let song_names=['Lilac','Celebrity','bibibi'];
-let song_links=['https://www.youtube.com/watch?v=v7bnOxV4jAc','https://www.youtube.com/watch?v=0-q1KafFCLU ','https://www.youtube.com/watch?v=nM0xDI5R50E'];
-let song_artist=['iu','iu','iu']
-let song_time=['3:34',"3:19","3:29"]
-let song_image=["https://doopiano.com/wp-content/uploads/2021/03/LILAC.jpg","https://hashtaglegend.com/wp-content/uploads/2021/01/IU-2.jpg","https://thebiaslistcom.files.wordpress.com/2018/10/iu-bbibbi.jpg "]
-let indexes =[0,1,2]
-
+// let song_names=['Lilac','Celebrity','bibibi'];
+// let song_links=['https://www.youtube.com/watch?v=v7bnOxV4jAc','https://www.youtube.com/watch?v=0-q1KafFCLU ','https://www.youtube.com/watch?v=nM0xDI5R50E'];
+// let song_artist=['iu','iu','iu']
+// let song_time=['3:34',"3:19","3:29"]
+// let song_image=["https://doopiano.com/wp-content/uploads/2021/03/LILAC.jpg","https://hashtaglegend.com/wp-content/uploads/2021/01/IU-2.jpg","https://thebiaslistcom.files.wordpress.com/2018/10/iu-bbibbi.jpg "]
+// let indexes =[0,1,2]
+let song1 = {name:"Lilac", link:"https://www.youtube.com/watch?v=v7bnOxV4jAc",artist:"iu", image:"https://doopiano.com/wp-content/uploads/2021/03/LILAC.jpg", time:"3:34",}
+let song2 = {name:"Celebrity", link:"https://www.youtube.com/watch?v=0-q1KafFCLU",artist:"iu", image:"https://hashtaglegend.com/wp-content/uploads/2021/01/IU-2.jpg", time:"3:19",}
+let song3 = {name:"Bibibi", link:"https://www.youtube.com/watch?v=nM0xDI5R50E",artist:"iu", image:"https://thebiaslistcom.files.wordpress.com/2018/10/iu-bbibbi.jpg ", time:"3:29",}
+let organizer = [song1,song2,song3]
 function displaySongInfo() {
-  indexes.forEach(function(one){
-      $(".images").append(`<img src="${song_image[one]}">`);
-      $(".songs").append(`<p>${song_names[one]}</p>`);
-      $(".artists").append(`<p>${song_artist[one]}</p>`);
-      $(".lengths").append(`<p>${song_time[one]}</p>`);
-      $(".links").append(`<a href="${song_links[one]}">Link here</a>`);
+  organizer.forEach(function(one){
+      $(".images").append(`<img src="${one.image}">`);
+      $(".songs").append(`<p>${one.name}</p>`);
+      $(".artists").append(`<p>${one.artist}</p>`);
+      $(".lengths").append(`<p>${one.time}</p>`);
+      $(".links").append(`<a href="${one.link}">Link here</a>`);
   });
 
 }
@@ -30,12 +33,20 @@ function emptySongInfo() {
 }
 
 function addSongInfo() {
-  indexes.push(indexes.length)
-  song_names.push($(".title").val());
-  song_links.push($(".link").val());
-  song_artist.push($(".artist").val());
-  song_time.push($(".length").val());
-  song_image.push($(".image").val());
+
+  let song_name = $(".title").val();
+  let song_link= $(".link").val();
+  let song_artist = $(".artist").val();
+  let song_time = $(".length").val();
+  let song_image = $(".image").val();
+  let new_song = {
+    name:song_name,
+    time:song_time,
+    link:song_link,
+    artist:song_artist,
+    image:song_image
+  };
+  organizer.push(new_song);
 }
 
 $(".add").click(function () {
